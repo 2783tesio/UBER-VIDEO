902f81641683ed2e4bd1d3856f51e8ce45e1e92a
Akash Avaya Behera <aakashbehera777@gmail.com>
2025-01-17 23:41:18 +0530

    Commit

--- Backend/app.js
+++ Backend/app.js
@@ -3,8 +3,14 @@ dotenv.config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
+const connectToDb = require("./db/db");
+const userRoutes = require("./routes/user.routes");
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
