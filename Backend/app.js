77af12142b26d132ab381713d6264c6254df9b8e
Akash Avaya Behera <aakashbehera777@gmail.com>
2025-01-25 00:05:00 +0530

    captain register

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 connectToDb();
 
 app.use(cors());
@@ -14,6 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
